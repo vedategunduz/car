9fe4923f995d70ec8924e307ea1690d0b7ba1c2d
vedategunduz <vedat.bilisim@outlook.com>
2023-12-30 21:56:30 +0300

    Addition of new files and changes

---
+++ assets/js/app.js
@@ -0,0 +1,5 @@
+import * as dropdown_all_funcitons from './modules/dropdown.mjs';
+import { capitalize } from './modules/text-utilities.mjs';
+import { initialize_accoridon } from './modules/accordion.mjs';
+import { load_theme_to_page, change_page_theme, set_theme_text_based_on_language } from './modules/theme.mjs';
+import { change_language } from './modules/languages.mjs';
---
+++ assets/js/modules/languages.mjs
@@ -0,0 +1,46 @@
+// Katı Mod'u (Strict Mode) etkinleştir.
+"use strict";
+
+import { capitalize } from "./text-utilities.mjs";
+import { set_theme_text_based_on_language } from "./theme.mjs";
+
+// localStorage'tan kaydedilmiş dil tercihini alır.
+const current_language = localStorage.getItem("language");
+
+// Sayfadaki dil değiştirme düğmesini seç
+const language_btn = document.getElementById("language_btn");
+
+
+// Sayfadaki dil değiştirme düğmelerini seç
+const language_change_buttons = document.querySelectorAll("#language_dropdown button");
+
+// Her bir düğme için bir olay dinleyici ekle
+language_change_buttons.forEach(button => {
+    // Düğmeye tıklandığında çalışacak işlevi belirle
+    button.addEventListener("click", function () {
+        // Dil değiştirme işlevini çağır ve yeni dil değerini iletilen veri özniteliğinden al
+        change_language(button.dataset.langValue);
+    });
+});
+
+// Dil değiştirme fonksiyonu. Varsayılan olarak "en" (İngilizce) dili seçilmiştir.
+// Bu fonksiyon, verilen dil koduna ve yönlendirme bilgisine göre dil dosyasını yükler.
+export function change_language(lang, direction = "assets/js/") {
+    (lang == null) ? lang = "en" : lang = lang;
+    localStorage.setItem("language", lang);
+
+    fetch(`${direction}languages/${lang}.json`)
+        .then(response => {
+            if (response.ok) {
+                return response.json();
+            }
+        })
+        .then(data => {
+            language_btn.innerText = `${capitalize(data.language)}` + ` ${data.lang}`.toUpperCase();
+        })
+    // Sayfa dili değiştirildiğinde, seçilen dil temelinde, tema butonu'nun metnini düzenler.
+    set_theme_text_based_on_language();
+}
+
+// Sayfa yüklendiğinde, kullanıcının tercih ettiği dil ile dil değiştirme fonksiyonunu çağırır.
+change_language(current_language);
